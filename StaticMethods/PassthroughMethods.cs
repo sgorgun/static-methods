af9509403b460ee0a6dfddb84c77940c9dcbcd9c
@gorgun.sergei <gorgun.sergei@student.ehu.lt>
2022-12-03 03:30:43 +0200

    Ready

--- StaticMethods/PassthroughMethods.cs
+++ StaticMethods/PassthroughMethods.cs
@@ -2,26 +2,59 @@
 {
     public static class PassthroughMethods
     {
-        // TODO #2-1: Add the static method here with name "ReturnInt" that gets "intParameter" parameter ("int" type) and returns it.
+        public static int ReturnInt(int intParameter)
+        {
+            return intParameter;
+        }
 
-        // TODO #2-2: Add the static method here with name "ReturnUnsignedInt" that gets "uintParameter" parameter ("uint" type) and returns it.
+        public static uint ReturnUnsignedInt(uint uintParameter)
+        {
+            return uintParameter;
+        }
 
-        // TODO #2-3: Add the static method here with name "ReturnLong" that gets "longParameter" parameter ("long" type) and returns it.
+        public static long ReturnLong(long longParameter)
+        {
+            return longParameter;
+        }
 
-        // TODO #2-4: Add the static method here with name "ReturnUnsignedLong" that gets "ulongParameter" parameter ("ulong" type) and returns it.
+        public static ulong ReturnUnsignedLong(ulong ulongParameter)
+        {
+            return ulongParameter;
+        }
 
-        // TODO #2-5: Add the static method here with name "ReturnFloat" that gets "floatParameter" parameter ("float" type) and returns it.
+        public static float ReturnFloat(float floatParameter)
+        {
+            return floatParameter;
+        }
 
-        // TODO #2-6: Add the static method here with name "ReturnDouble" that gets "doubleParameter" parameter ("double" type) and returns it.
+        public static double ReturnDouble(double doubleParameter)
+        {
+            return doubleParameter;
+        }
 
-        // TODO #2-7: Add the static method here with name "ReturnDecimal" that gets "decimalParameter" parameter ("decimal" type) and returns it.
+        public static decimal ReturnDecimal(decimal decimalParameter)
+        {
+            return decimalParameter;
+        }
 
-        // TODO #2-8: Add the static method here with name "ReturnString" that gets "stringParameter" parameter ("string" type) and returns it.
+        public static string ReturnString(string stringParameter)
+        {
+            return stringParameter;
+        }
 
-        // TODO #2-9: Add the static method here with name "ReturnChar" that gets "charParameter" parameter ("char" type) and returns it.
+        public static char ReturnChar(char charParameter)
+        {
+            return charParameter;
+        }
 
-        // TODO #2-10: Add the static method here with name "ReturnByte" that gets "byteParameter" parameter ("byte" type) and returns it.
+        public static byte ReturnByte(byte byteParameter)
+        {
+            return byteParameter;
+        }
 
-        // TODO #2-11: Add the static method here with name "ReturnBool" that gets "boolParameter" parameter ("bool" type) and returns it.
+        public static bool ReturnBool(bool boolParameter)
+        {
+            return boolParameter;
+        }
     }
 }
--- StaticMethods/ReturningMethods.cs
+++ StaticMethods/ReturningMethods.cs
@@ -1,27 +1,62 @@
-﻿namespace StaticMethods
+﻿using System;
+
+namespace StaticMethods
 {
     public static class ReturningMethods
     {
-        // TODO #1-1: Add the static method here with name "ReturnInt" that should return -1234567 literal (int return type).
+        public static int ReturnInt()
+        {
+            return -1234567;
+        }
 
-        // TODO #1-2: Add the static method here with name "ReturnUnsignedInt" that should return 1234567u literal (uint return type).
+        public static uint ReturnUnsignedInt()
+        {
+            return 1234567u;
+        }
 
-        // TODO #1-3: Add the static method here with name "ReturnLong" that should return -987654321L literal (long return type).
+        public static long ReturnLong()
+        {
+            return -987654321L;
+        }
 
-        // TODO #1-4: Add the static method here with name "ReturnUnsignedLong" that should return 987654321uL literal (ulong return type).
+        public static ulong ReturnUnsignedLong()
+        {
+            return 987654321uL;
+        }
 
-        // TODO #1-5: Add the static method here with name "ReturnFloat" that should return 1234.567f literal (float return type).
+        public static float ReturnFloat()
+        {
+            return 1234.567f;
+        }
 
-        // TODO #1-6: Add the static method here with name "ReturnDouble" that should return -9876.54321 literal (double return type).
+        public static double ReturnDouble()
+        {
+            return -9876.54321;
+        }
 
-        // TODO #1-7: Add the static method here with name "ReturnDecimal" that should return -123456789.987654321m literal (decimal return type).
+        public static decimal ReturnDecimal()
+        {
+            return -123456789.987654321m;
+        }
 
-        // TODO #1-8: Add the static method here with name "ReturnString" that should return "Hello, world!" literal (string return type).
+        public static string ReturnString()
+        {
+            return "Hello, world!";
+        }
 
-        // TODO #1-9: Add the static method here with name "ReturnChar" that should return 'A' literal (char return type).
+        public static char ReturnChar()
+        {
+            return 'A';
+        }
 
-        // TODO #1-10: Add the static method here with name "ReturnByte" that should return 0xAB literal (byte return type).
+        public static byte ReturnByte()
+        {
+            return 0xAB;
+        }
 
-        // TODO #1-11: Add the static method here with name "ReturnBool" that should return true literal (bool return type).
+        public static bool ReturnBool()
+        {
+            return true;
+        }
     }
 }
